4bb358da6fc8a49915d190544d059aea8842d876
YangLi <liang.900503@gmail.com>
2014-07-03 10:48:45 -0700

    Update PalindromePartitioningII.java

--- PalindromePartitioningII.java
+++ PalindromePartitioningII.java
@@ -10,5 +10,17 @@
 
 intpublic class Solution {
     public int minCut(String s) {
-i    }int
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        int count = new int[s.length()+1];
+        for(int i=s.length()-1; i>=0; i--) {
+            count[i] = Integer.MAX_VALUE;
+            for(j=i; j<s.length(); j++) {
+                if(s.charAt(i)==s.charAt(j) && (j-i<2 || dp[i+1][j-1])) {
+                    dp[i][j] = true;
+                    count[i] = Math.min(count[j+1]+1, count[i]);
+                }
+            }
+        }
+        return count[0]-1;
+    }
 }
