3967756f7c6044fcdf2c25a754e9d678aa20b574
YangLi <liang.900503@gmail.com>
2014-07-11 12:54:46 -0700

    Update Sqrt-x.java

--- Sqrt-x.java
+++ Sqrt-x.java
@@ -2,10 +2,20 @@
 
 // Compute and return the square root of x.
 
-public class Solution {
+
 public class Solution {
     public int sqrt(int x) {
-    public int sqrt(int x) {
-    
+        long low = 0;
+        long high = x/2+1;
+        while(low<=hight) {
+            long mid = (low+high)/2;
+            long sqr = mid * mid;
+            if(sqr==x) return (int)mid;
+            else if(sqr<x) low = mid+1;
+            else high = mid-1;
+        }
+        return (int)high;
+    ｝
+｝
     
     
