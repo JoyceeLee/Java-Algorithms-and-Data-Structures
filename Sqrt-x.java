799c86f8c1434a88879d396802fd294f5571f1d2
YangLi <liang.900503@gmail.com>
2014-07-11 12:30:08 -0700

    Create Sqrt-x.java

---
+++ Sqrt-x.java
@@ -0,0 +1,11 @@
+// Implement int sqrt(int x).
+
+// Compute and return the square root of x.
+
+public class Solution {
+public class Solution {
+    public int sqrt(int x) {
+    public int sqrt(int x) {
+    
+    
+    
