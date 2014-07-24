7063fd99c1597ecdbcf2b7242661c60200903848
YangLi <liang.900503@gmail.com>
2014-07-23 22:05:54 -0700

    Update InterleavingString.java

--- InterleavingString.java
+++ InterleavingString.java
@@ -10,6 +10,11 @@
  */
 
 
+/**
+ *  DP
+ * Time Complexity : O(m*n)
+ * Space Complexity : O(min(m, n))
+ */
 public class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         if(s1.length()+s2.length()!=s3.length()) return false;
