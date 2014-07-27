9419c6e82a925399e8e05b4cedbe1dd67da2dcc9
YangLi <liang.900503@gmail.com>
2014-07-26 19:33:31 -0700

    Update WordLadderII.java

--- WordLadderII.java
+++ WordLadderII.java
@@ -11,15 +11,16 @@
  * dict = ["hot","dot","dog","lot","log"]
  * Return
  * [
- * ["hit","hot","dot","dog","cog"],
- * ["hit","hot","lot","log","cog"]
+ *  ["hit","hot","dot","dog","cog"],
+ *  ["hit","hot","lot","log","cog"]
  * ]
  * Note:
  * All words have the same length.
  * All words contain only lowercase alphabetic characters.
  */
  
- public class Solution {
+
+public class Solution {
     public List<List<String>> findLadders(String start, String end, Set<String> dict) {
         List<List<String>> ret = new ArrayList<List<String>>();
         List<String> tmp = new ArrayList<String>();
@@ -31,10 +32,65 @@
             return ret;
         }
         HashMap<String, ArrayList<String>> pre = new HashMap<String, ArrayList<String>>();
-        pre.put(start, new ArrayList<String>);
-        pre.put(end, new ArrayList<String>);
+        pre.put(start, new ArrayList<String>());
+        pre.put(end, new ArrayList<String>());
         for(String str : dict) {
-            pre.put(str, );
+            pre.put(str, new ArrayList<String>());
+        }
+        Queue<String> que = new LinkedList<String>();
+        que.add(start);
+        ArrayList<String> curLevel = new ArrayList<String>();
+        int levelSize = 0;
+        while(!que.isEmpty()) {
+            curLevel.clear();
+            levelSize = que.size();
+            for(int i=0; i<levelSize; i++) {
+                String top = que.poll();
+                if(dict.contains(top))
+                    dict.remove(top);
+                curLevel.add(top);
+            }
+            for(String str : curLevel) {
+                char[] curStr = str.toCharArray();
+                for(int i=0; i<str.length(); i++) {
+                    char org = curStr[i];
+                    for(char j='a'; j<='z'; j++) {
+                        if(j==org) continue;
+                        curStr[i] = j;
+                        String tmpStr = new String(curStr);
+                        if(!str.equals(start) && tmpStr.equals(end)) {
+                            que.add(tmpStr);
+                            pre.get(tmpStr).add(str);
+                        } else {
+                            if(dict.contains(tmpStr)) {
+                                if(!que.contains(tmpStr))
+                                    que.add(tmpStr);
+                                pre.get(tmpStr).add(str);
+                            }
+                        }
+                    }
+                    curStr[i] = org;
+                }
+            }
+            if(que.contains(end))
+                break;
+        }
+        tmp.add(end);
+        buildLadder(ret, tmp, pre, start, end);
+        return ret;
+    }
+    public void buildLadder(List<List<String>> ret, List<String> tmp, HashMap<String, ArrayList<String>> pre,
+                            String start, String end) {
+        if(end.equals(start)) {
+            List<String> tmp2 = new ArrayList<String>(tmp);
+            Collections.reverse(tmp2);
+            ret.add(tmp2);
+            return;
+        }
+        for(String str : pre.get(end)) {
+            tmp.add(str);
+            buildLadder(ret, tmp, pre, start, str);
+            tmp.remove(tmp.size()-1);
         }
     }
 }
