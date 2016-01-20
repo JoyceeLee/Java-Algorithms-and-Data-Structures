8d4474a30a5274507a8bd06842889c1c2f2353c2
YangLi <liang.900503@gmail.com>
2016-01-19 22:31:40 -0600

    Update RotateImage.java

--- RotateImage.java
+++ RotateImage.java
@@ -8,15 +8,16 @@
 
 public class Solution {
     public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 1) return;
+        
         int n = matrix.length;
-        if(n<=1) return;
-        for(int i=0; i<n; i++) {
-            for(int j=i; j<n-i-1; j++) {
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - i - 1; j++) {
                 int tmp = matrix[i][j];
-                matrix[i][j] = matrix[n-j-1][i];
-                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
-                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
-                matrix[j][n-i-1] = tmp;
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n -i -1] = tmp;
             }
         }
     }
