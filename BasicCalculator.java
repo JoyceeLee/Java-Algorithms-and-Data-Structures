d893c671a644000ff89822f676530bd410800146
YangLi <liang.900503@gmail.com>
2016-05-24 20:35:17 -0500

    Update BasicCalculator.java

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -11,3 +11,31 @@ Some examples:
 "(1+(4+5+2)-3)+(6+8)" = 23
 */
 
+public class Solution {
+    public int calculate(String s) {
+        int len = s.length(), sign = 1, result = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < len; i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                int tmp = 0;
+                do {
+                    tmp = tmp * 10 + (s.charAt(i) - '0');
+                    i++;
+                } while (i < len && Character.isDigit(s.charAt(i)));
+                result += tmp * sign;
+            } else if (s.charAt(i) == '+') {
+                sign = 1;
+            } else if (s.charAt(i) == '-') {
+                sign = -1;
+            } else if (s.charAt(i) == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (s.charAt(i) == ')') {
+                result = result * stack.pop() + stack.pop();
+            }
+        }
+        return result;
+    }
+}
